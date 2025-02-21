e20b3662a059c0feabaf3bfa29312fab63b1ccd7
Manuel Bax <baccimanuel01@gmail.com>
2025-02-21 02:26:52 +0100

    Aggiornato codice

--- Alarm_Sensor/Alarm_Sensor.ino
+++ Alarm_Sensor/Alarm_Sensor.ino
@@ -3,34 +3,43 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 #define alarm 6            // buzzer
 #define motionSensor 7     // pir_sensor
-#define led 13           // LED
+#define led 13             // LED
 
 void setup() {
-  //impostzione delle uscite dei componenti
-    pinMode(alarm, OUTPUT); 
-    pinMode(motionSensor, INPUT);
-    pinMode(led, OUTPUT);
+  // Impostazione delle uscite dei componenti
+  pinMode(alarm, OUTPUT); 
+  pinMode(motionSensor, INPUT);
+  pinMode(led, OUTPUT);
 
-    lcd.begin(16, 2);
-    lcd.print("Sistema Pronto");
-    delay(2000);
-    lcd.clear();
+  lcd.begin(16, 2);
+  lcd.setCursor(1, 0);   
+  lcd.print("Sistema");
+  lcd.setCursor(0, 1); 
+  lcd.print("Attivato"); 
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
-    if (digitalRead(motionSensor) == HIGH) {
-        digitalWrite(alarm, LOW);
-        digitalWrite(led, HIGH);
-         lcd.clear();                       // Pulisce lo schermo
-        lcd.print("Movimento rilevato");
-        delay(100);
-        digitalWrite(alarm, HIGH);
-        digitalWrite(led, LOW);
-        delay(100);
-    } else {
-      //componenti in riposo
-        digitalWrite(alarm, LOW);
-        digitalWrite(led, LOW);
-        lcd.print("sistema disattivato");
-    }
+  if (digitalRead(motionSensor) == HIGH) {
+    digitalWrite(alarm, LOW);
+    digitalWrite(led, HIGH);
+    lcd.clear();            // Pulisce lo schermo       
+    lcd.setCursor(1, 0);               
+    lcd.print("Movimento");
+    lcd.setCursor(0, 1);
+    lcd.print("Rilevato"); 
+    delay(100);
+    digitalWrite(alarm, HIGH);
+    digitalWrite(led, LOW);
+    delay(100);
+  } else {
+    // Componenti in riposo
+    digitalWrite(alarm, LOW);
+    digitalWrite(led, LOW);
+    lcd.setCursor(1, 0);   
+    lcd.print("Nessun");
+    lcd.setCursor(0, 1); 
+    lcd.print("Rilevamento");
+  }
 }
